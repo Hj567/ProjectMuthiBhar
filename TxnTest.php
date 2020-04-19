73ed287453c27a5d3906f4ebfaa6e9f2f1e7478b
Hardik Jain <34153257+Hj567@users.noreply.github.com>
2020-04-19 22:59:21 +0530

    yo boyz

--- TxnTest.php
+++ TxnTest.php
@@ -4,6 +4,7 @@
 	header("Expires: 0");
 
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -11,109 +12,81 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
 <meta name="GENERATOR" content="Evrsoft First Page">
-<link rel="stylesheet" type="text/css" href="donationForm.css">
+<link rel="stylesheet" type="text/css" href="donationForm1.css">
 </head>
 <body>
 
-	 <div class="containerSide">
+<section>
+	 
+<div class="svg-wrapper">
 	
-<div class="membersContainer">
+<div class="svg">
 	
-<p class="hello"> 260</p> 
-<p> Donated</p>
+	<img src="undraw_gifts_btw0 10.05.46 PM.svg">
 
-
-
-</div>
-
-<div class="projectsContainer">
-	
-	<p class="hello"> 200</p> 
-<p> PEOPLE FED</p>
-
-
-</div>
-
-<div class="visitorsContainer">
-	
-	<p class="hello"> 380</p> 
-<p> LIVES TOUCHED</p>
-
-<script type="text/javascript">
-	
-	$(".hello").counterUp({delay:10,time:2000})
-
-</script>
-	
 </div>
 
 
 </div>
 
-
-
-	<form method="post" action="pgRedirect.php">
+	
 	
 						
 <div class="wrapper">
 
 
-<div class="orderID yolo">
+<form method="post" action="pgRedirect.php">
 				
+				<div>
 					
 						<input id="ORDER_ID"tabindex="1" maxlength="20" size="20"
 						name="ORDER_ID" autocomplete="off"
-						value="<?php echo  "ORDS" . rand(10000,99999999)?>" readonly="readonly>
+						value="<?php echo  "ORDS" . rand(10000,99999999)?>" readonly="readonly" type="text">
+
 
 
 </div>
 
-<div class="nameUserDiv yolo">
 						
-						<label id="helloWorld">
-
-						<span>Name</span>
-</label>
-
+						
+<div>
 						<input title="nameUser" 
-						type="text" name="nameUser" class="name" id="name" required="required">
+						type="text" name="nameUser" class="name" id="name" required="required" placeholder="Name">
 
-					</div>
+					
 
-					<div class="emailDiv yolo">
-<label>
-						<span>Email Address</span>
+			</div>
 
-</label>
+			<div>		
+
 
 						<input title="userEmail" 
-						type="text" name="userEmail" required="required">
+						type="text" name="userEmail" required="required" placeholder="E-mail">
 					
-					</div>
+					
 
-<div class="donationAmt yolo">
+</div>
 
-<label>
-						<span> Donation Amount</span>
-
-</label>
+<div>
 
 						<input title="TXN_AMOUNT" tabindex="10"
 						type="text" name="TXN_AMOUNT"
-						required="required">
+						required="required" placeholder="Donation Amount">
 
 					
 
-					</div>
+			</div>	
 
-	<input value="Donate Now" type="submit"	onclick="" class="submit" style="background-image: linear-gradient(120deg, #f093fb 0%, #f5576c 100%); height: 55px; width:400px; border-bottom: none;">
+<div class="submitBtn">
+	
+	<input value="Donate Now" type="submit"	onclick="" class="submit">
 			
-
+</div>
 
 	<input id="CUST_ID" tabindex="2" maxlength="12" size="12" name="CUST_ID" autocomplete="off" value="CUST001" hidden>
 
@@ -126,9 +99,13 @@
 
 					
 
-					</div>
+					
 			
 	</form>
+
+	</div>
+
+	</section>
 </body>
 
 <script type="text/javascript">
